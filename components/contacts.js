a2096d8db14a297f7ebaee34173cbfe275a99738
Dima <dimagumball@gmail.com>
2021-09-07 17:01:29 +0300

    link in new windows

--- components/contacts.js
+++ components/contacts.js
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 export default function Contacts() {
   return (
     <div className="numabers">
-      <div style={{display:"flex", justifyContent:"center",alignItems:"center"}}><FaInstagram size={40}/><a href={offlineContact.number[0]} style={{textDecoration:"none",color: "#666666"}}>@kupibest</a></div>
+      <div style={{display:"flex", justifyContent:"center",alignItems:"center"}}><FaInstagram size={40}/><a href={offlineContact.number[0]} style={{textDecoration:"none",color: "#666666"}}  target="_blank">@kupibest</a></div>
       <div style={{display:"flex", justifyContent:"center",alignItems:"center", marginLeft:"20px"}}><a href={"tel:"+offlineContact.number[1]} style={{textDecoration:"none",color: "#666666"}}>{offlineContact.number[1]}</a></div>
       <style jsx>{`
           .numabers {
